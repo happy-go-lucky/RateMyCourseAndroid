b65c19d20b4c6182eff738f0cdeeafaeb00302a9
Brodie Heywood <brodieheywood@gmail.com>
2019-04-03 23:07:10 -0700

    Add rating metric fields

--- app/src/main/java/com/example/happi/ratemycourse/RatingDataModel.java
+++ app/src/main/java/com/example/happi/ratemycourse/RatingDataModel.java
@@ -1,4 +1,17 @@
 package com.example.happi.ratemycourse;
 
+/**
+ * Rating Database Model
+ *
+ * @author Brodie Heywood
+ * @version 0.1
+ */
 public class RatingDataModel {
+
+    // Rating metrics
+    private int _homework;
+    private int _reading;
+    private int _usefulness;
+    private int _stress;
+
 }
