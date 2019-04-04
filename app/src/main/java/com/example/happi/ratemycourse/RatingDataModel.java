a53145ffb39387e17c99785b2af2e500b78b02d7
Brodie Heywood <brodieheywood@gmail.com>
2019-04-03 23:21:43 -0700

    Add constructor

--- app/src/main/java/com/example/happi/ratemycourse/RatingDataModel.java
+++ app/src/main/java/com/example/happi/ratemycourse/RatingDataModel.java
@@ -14,4 +14,22 @@ public class RatingDataModel {
     private int _usefulness;
     private int _stress;
 
+    /**
+     * RatingDataModel Constructor
+     *
+     * @param homework user-inputted amount of homework on 10 point scale
+     * @param reading amount of reading required from course on 10 point scale
+     * @param usefulness usefulness of course on 10 point scale
+     * @param stress stressfulness of course on 10 point scale
+     */
+    public RatingDataModel(int homework,
+                           int reading,
+                           int usefulness,
+                           int stress) {
+        _homework = homework;
+        _reading = reading;
+        _usefulness = usefulness;
+        _stress = stress;
+    }
+
 }
