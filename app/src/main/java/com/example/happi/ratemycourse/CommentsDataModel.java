b296785675d6920125ea73b082cd2d815206d5ad
craiggoodison <cgoodison1@my.bcit.ca>
2019-04-04 14:36:54 -0700

    Add accessors and mutators

--- app/src/main/java/com/example/happi/ratemycourse/CommentsDataModel.java
+++ app/src/main/java/com/example/happi/ratemycourse/CommentsDataModel.java
@@ -1,4 +1,33 @@
 package com.example.happi.ratemycourse;
 
+//import android.util.Log;
+
+
 public class CommentsDataModel {
+    private final String LOG_TAG = "UserDataModel";
+    private int _commentId;
+    private String _comments;
+
+
+    public CommentsDataModel(int commentId, String comments) {
+        _commentId = commentId;
+        _comments = comments;
+    }
+
+    public int get_CommentId() {
+        return _commentId;
+    }
+
+    public String get_Comments() {
+        return _comments;
+    }
+
+    public void set_CommentId(int commentId) {
+        _commentId =commentId;}
+
+    public void set_Comments(String comments) {
+        _comments =comments;}
+
 }
+
+
